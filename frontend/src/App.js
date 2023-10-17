1740d9ac8d6fcb61c2f278dd709f89afdc9b95a7
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2023-10-17 23:04:52 +0200

    Added frontend folder with react and tailwindcss setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <header className="App-header">
+        <button
+          className="bg-indigo-600 hover:bg-indigo-300 text-white font-semibold py-4 px-4 rounded transform hover:-translate-y-1 hover:-translate-x-1 hover:scale-110 transition duration-300 ease-in-out">
+          test
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
