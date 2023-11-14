eacec552dc3526bbcdae81eedb4af91fde3dcdb8
KacperWojdak <kacper.wojdak@interia.pl>
2023-11-14 20:28:09 +0100

    Update animation and header

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -28,7 +28,7 @@ const Banner = ({ backgroundImage }) => {
   return (
     <section className="relative w-full min-h-screen bg-cover flex justify-center items-center" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <h2 className="text-white text-8xl uppercase text-center leading-snug font-semibold">
-        Welcome to <br /> Kibol<span className="text-custom-tan">APP</span>
+        Witaj w <br /> Kibol<span className="text-custom-tan">APP</span>
       </h2>
     </section>
   );
@@ -36,26 +36,29 @@ const Banner = ({ backgroundImage }) => {
 
 return (
   <>
-    <header className={`${isSticky ? 'bg-custom-brown' : 'bg-black bg-opacity-50'} fixed top-0 left-0 w-full flex items-center justify-between px-10 py-2 z-10 transition-all duration-300 ease-in-out`}>
-      <div className="text-white uppercase text-2xl font-bold">
-        <img src={isSticky ? data[1] : data[0]} alt="KibolAPP Logo" className="h-16 transition-all duration-300 ease-in-out" />
+    <header className={`${isSticky ? 'bg-custom-brown' : 'bg-black bg-opacity-50'} fixed top-0 left-0 w-full z-10 transition-all ease-in-out duration-1000`}>
+      <div className="flex items-center justify-between w-full px-10 py-5">
+        <div className="flex-shrink-0">
+          <img src={data[0]} alt="KibolAPP Logo" className={`absolute top-0 left-0 h-16 object-contain transition-opacity ease-in-out duration-1000 ${isSticky ? 'opacity-0' : 'opacity-100'}`}/>
+          <img src={data[1]} alt="Sticky Logo" className={`absolute top-0 left-0 h-16 object-contain transition-opacity ease-in-out duration-1000 ${!isSticky ? 'opacity-0' : 'opacity-100'}`}/>
+        </div>
+        <nav className='flex-grow'>
+          <ul className="flex justify-end">
+            <li className="text-white mx-4 font-semibold">
+              <a className="hover:text-gray-300 text-green-500"><Link to="/home">Start</Link></a>
+            </li>
+            <li className="text-white mx-4 font-semibold">
+              <a className="hover:text-gray-300"><Link to="/app">Lokalizacja klubów</Link></a>
+            </li>
+            <li className="text-white mx-4 font-semibold">
+              <a href="#" className="hover:text-gray-300">Spis klubów</a>
+            </li>
+            <li className="text-white mx-4 font-semibold">
+              <a className="hover:text-gray-300"><Link to="/auth">Logowanie / Rejestracja</Link></a>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav>
-        <ul className="flex">
-          <li className="text-white mx-4 font-semibold">
-            <a className="hover:text-gray-300 text-green-500"><Link to="/home">Start</Link></a>
-          </li>
-          <li className="text-white mx-4 font-semibold">
-            <a className="hover:text-gray-300"><Link to="/app">Lokalizacja klubów</Link></a>
-          </li>
-          <li className="text-white mx-4 font-semibold">
-            <a href="#" className="hover:text-gray-300">Spis klubów</a>
-          </li>
-          <li className="text-white mx-4 font-semibold">
-            <a className="hover:text-gray-300"><Link to="/auth">Logowanie / Rejestracja</Link></a>
-          </li>
-        </ul>
-      </nav>
     </header>
 
     <Banner backgroundImage={data[2]} />
@@ -65,10 +68,10 @@ return (
       <div className="md:flex-1 md:mr-6">
         <h2 className="text-3xl font-bold mb-4">Czym jest kibolApp - ABOUT</h2>
           <p className="text-lg mb-6">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            KibolApp to innowacyjna strona stworzona dla pasjonatów piłki nożnej i kibiców, którzy chcą poznać kulisy relacji między klubami.
           </p>
           <p className="text-lg mb-6">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus magni a ea earum! Facilis praesentium at est laborum, non dolorem sit voluptatum tenetur magni suscipit eaque dolor deserunt, sint impedit!
+            Aplikacja umożliwia śledzenie lokalizacji klubów, a także dostarcza informacje na temat przyjaźni i konfliktów między różnymi drużynami.
           </p>
       </div>
       <div className="md:flex-1 max-w-md mx-auto">
@@ -80,7 +83,7 @@ return (
     <section className="bg-custom-sand py-16">
       <div className="flex justify-center">
         <div className="max-w-2xl w-full">
-          <h3 className="text-white text-3xl text-center mb-10 font-bold">Feel Free to <span className="text-custom-brown uppercase font-bold">Contact Us</span></h3>
+          <h3 className="text-white text-3xl text-center mb-10 font-bold">Masz pytania? <span className="text-custom-brown uppercase font-bold">Skontaktuj się z nami</span></h3>
             <form className="flex flex-col items-center">
               <div className="flex w-full mb-6">
                 <input type="text" placeholder="Name" className="flex-1 bg-transparent border-b border-black text-black px-2 py-1 outline-none placeholder-black placeholder-opacity-50 mr-4" />
