f86c745e68115cdd91db331c56c2d07f01486436
Dagmara Skulimowska <dagmaraskul@wp.pl>
2023-11-17 12:52:39 +0100

    Revert "Merge branch 'main' into 15-docker-fix"
    This reverts commit de4dce985c9023a4a4ba83526fad25bb3015d358, reversing
    changes made to e491c460ea41f81cd5ec91d3afc70820ad4a4765.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from './components/Header';
-import Map from './components/Map';
 
 const App = () => {
-  return (
-    <>
-      <div className="min-h-fit bg-custom-gray">
+    return (
+        <>
+        <div className="min-h-screen bg-custom-gray flex flex-col">
         <Header />
-        <div className="flex justify-beetween">
-        <Map />
-        <div className="p-4">
-        <div className="flex flex-col items-start mt-8 ml-8">
-            <label htmlFor="search" className="text-white mb-2">
-              Wyszukaj klub:
-            </label>
-            <input
-              type="text"
-              id="search"
-              className="border rounded px-2 py-1 mb-4"
-              placeholder="Wyszukaj klub..."
-            />
-             <button className="bg-custom-brown text-white px-4 py-2 rounded">
-                  Szukaj
-             </button>
+          <div className='text-white'>
+          <Link to="/">Home</Link>
           </div>
-        </div>
-        </div>
-      </div>
-    </>
-  );
+          </div>
+        </>
+      );
 };
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './router.js'
-import { ContextProvider } from './contexts/ContextProvider.js'
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
+import AuthForm from './AuthForm';
+import Home from './Home'; 
 import reportWebVitals from './reportWebVitals';
-
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <ContextProvider>
-    <RouterProvider router={router}/>
-    </ContextProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<AuthForm />} />
+        <Route path="/app" element={<App />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
