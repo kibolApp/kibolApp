e0012976225cc9bd95b9e6d081ee110a0584a89f
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2023-11-24 02:04:03 +0100

    small header changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,12 @@ import UserHeader from './components/UserHeader';
 const App = () => {
   return (
     <>
-      <div className="min-h-fit bg-custom-gray">
+      <div className="flex flex-col min-h-screen bg-custom-gray">
         <UserHeader />
         <div className="flex justify-beetween">
         <Map />
         <div className="p-4">
-        <div className="flex flex-col items-start mt-8 ml-8">
+        <div className="flex flex-col items-start mt-2 ml-4">
             <label htmlFor="search" className="text-white mb-2 bg-custom-brown rounded">
              <p> Aby znaleźć najbliższy klub piłkarski </p>
              <p> Kliknij prawy przycisk myszki w dowolnym </p>
--- frontend/src/components/UserHeader.js
+++ frontend/src/components/UserHeader.js
@@ -17,12 +17,19 @@ const data = [
     ];
     
 return (
-    <header className="flex justify-between items-center p-5 bg-custom-brown">
-      <button>
-        <Link to="/">
-          <img src={data[0]} alt="LeftIcon" className="h-16 bg-custom-gray rounded-full shadow-lg drop-shadow-lg hover:opacity-80 hover:scale-110 hover:bg-custom-gray-dark transition duration-300 ease-in-out" />
-        </Link>
-      </button>
+  <header className="flex top-0 left-0 w-full justify-between items-center bg-custom-brown my-1 px-10 py-4 z-10">
+  <div className="flex items-center">
+    <button>
+      <Link to="/">
+        <img src={data[0]} alt="LeftIcon" className="h-16 bg-custom-gray rounded-xl shadow-lg drop-shadow-lg hover:opacity-80 hover:scale-110 hover:bg-custom-gray-dark transition duration-300 ease-in-out" />
+      </Link>
+    </button>
+    <div className="flex flex-col justify-center px-4">
+      <button onClick={() => changeLanguage('pl')}><img src={data[2]} alt="Poland Flag" className="h-8 w-12 object-cover mb-2 rounded-md border-solid"/></button>
+      <button onClick={() => changeLanguage('en')}><img src={data[3]} alt="GB Flag" className="h-8 w-12 object-cover rounded-md border-solid"/></button>
+    </div>
+  </div>
+
       
       <div className="flex items-center space-x-4">
       <nav className='flex-grow'>
@@ -41,11 +48,6 @@ return (
             </li>
           </ul>
         </nav>
-        <div className="flex flex-col justify-center">
-          <button onClick={() => changeLanguage('pl')}><img src={data[2]} alt="Poland Flag" className="h-8 w-12 object-cover mb-2 rounded-md border-solid"/></button>
-          <button onClick={() => changeLanguage('en')}><img src={data[3]} alt="GB Flag" className="h-8 w-12 object-cover rounded-md border-solid"/></button>
-        </div>
-        
         {/* User Logo */}
         <button>
         <img src={data[1]} alt="RightIcon" className="h-20" />
