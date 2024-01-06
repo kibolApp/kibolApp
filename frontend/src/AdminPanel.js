cb8b9825cb09a1efac84dfdf51be2f638079d689
Tomasz Barczycki <115528520+TomaszBarczycki@users.noreply.github.com>
2024-01-06 13:19:59 +0100

    Revert "revert2"
    This reverts commit 0295822b01af435cd65e5774486d093d7e1375b1.

--- frontend/src/AdminPanel.js
+++ frontend/src/AdminPanel.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import {Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 import axiosClient from './axiosClient';
 import UserHeader from './components/UserHeader';
 import ClubManagement from './components/ClubsManagement';
 import UserManagement from './components/UsersManagement';
+import LoadingScreen from './LoadingScreen';
 
 const AdminPanel = () => {
+  const navigate = useNavigate();
   const [selectedTab, setSelectedTab] = useState('users');
   const [user, setUser] = useState(null);
 
@@ -34,20 +36,20 @@ const AdminPanel = () => {
     <div className="min-h-screen bg-custom-gray flex flex-col items-center">
       <UserHeader />
       <div className="mb-4 mt-6 flex">
-      <button
-  onClick={() => setSelectedTab('users')}
-  className={`mr-2 px-4 py-2 rounded-md ${
-    selectedTab === 'users' ? 'px-3 py-1 mx-1 text-white border rounded cursor-pointer transition duration-300 ease-in-out bg-custom-olive hover:bg-custom-olive hover:text-white' : 'bg-custom-olive hover:text-white'
-  }`}
->
-  Użytkownicy
-</button>
-<button
-  onClick={() => setSelectedTab('clubs')}
-  className={`px-4 py-2 rounded-md ${
-    selectedTab === 'clubs' ? 'px-3 py-1 mx-1 text-white border rounded cursor-pointer transition duration-300 ease-in-out bg-custom-olive hover:bg-custom-olive hover:text-white' : 'bg-custom-olive hover:text-white'
-  }`}
->
+        <button
+          onClick={() => setSelectedTab('users')}
+          className={`mr-2 px-4 py-2 rounded-md ${
+            selectedTab === 'users' ? 'px-3 py-1 mx-1 text-white border rounded cursor-pointer transition duration-300 ease-in-out bg-custom-olive hover:bg-custom-olive hover:text-white' : 'bg-custom-olive hover:text-white'
+          }`}
+        >
+          Użytkownicy
+        </button>
+        <button
+          onClick={() => setSelectedTab('clubs')}
+          className={`px-4 py-2 rounded-md ${
+            selectedTab === 'clubs' ? 'px-3 py-1 mx-1 text-white border rounded cursor-pointer transition duration-300 ease-in-out bg-custom-olive hover:bg-custom-olive hover:text-white' : 'bg-custom-olive hover:text-white'
+          }`}
+        >
           Kluby
         </button>
       </div>
