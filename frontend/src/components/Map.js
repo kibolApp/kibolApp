cd7ce996a7079d094be60b780589afafd7d49747
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2024-01-10 00:51:30 +0100

    Developing Areas for each and every team

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents, Polygon, Rectangle, Tooltip } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents, Polygon, Tooltip } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import MarkerClusterGroup from 'react-leaflet-cluster';
 import axiosClient from "../axiosClient";
@@ -17,29 +17,49 @@ const CustomMap = () => {
   const [markersData, setMarkersData] = useState([]);
   const userLocationIcon = new Icon({ iconUrl: 'https://i.imgur.com/iulwF9C.png', iconSize: [32, 32] });
 
+  const areasData = [
+    {
+      team: "Cracovia Kraków",
+      areas: [
+        [
+          { lat: 50.06213458295723, lng: 19.934340315138286 },
+        { lat: 50.05557189565738, lng: 19.885426591545496 },
+        { lat: 50.03185069902199, lng: 19.88848488407004 },
+        { lat: 50.0251514855436, lng: 19.939623159272628 },
+        { lat: 50.04163742969706, lng: 19.948513027112114 },
+        { lat: 50.05913458295723, lng: 19.934340315138286 },
+        ]],
+    },
+    {
+      team: "Wisła Kraków",
+      areas: [
+        [
+         { lat: 50.05721491636078, lng: 19.907203231912007 },
+        { lat: 50.063258155128466, lng: 19.94805494702055 },
+        { lat: 50.085634888833, lng: 19.9592726055117 },
+        { lat: 50.11930541531717, lng: 19.90924538333519 },
+        { lat: 50.086813198755834, lng: 19.869409276652732 },
+        { lat: 50.059510553034045, lng: 19.878830850451436 },
+        { lat: 50.05721491636078, lng: 19.907203231912007 },
+        ]
+      ],
+    },
+  ];
+
   useEffect(() => {
     axiosClient.get('/clubs')
       .then(({ data }) => {
-        console.log(data);
-        const transformedData = data.map(club => ({
-          team: club.team,
-          location: [club.latitude, club.longitude],
-          address: club.address,
-          icon: new Icon({ iconUrl: club.url_logo, iconSize: [46, 46] }),
-          url:"/clubpage/" + club.url,
-          //area: club.area, TU będzie
-          areas: [
-            [
-              [club.latitude - 0.01, club.longitude - 0.01],
-              [club.latitude + 0.01, club.longitude + 0.01],
-            ],
-            [
-              [club.latitude, club.longitude - 0.02],
-              [club.latitude + 0.02, club.longitude],
-              [club.latitude, club.longitude + 0.02],
-            ],
-          ],
-        }));
+        const transformedData = data.map(club => {
+          const clubAreas = getClubAreas(club.team);
+          return {
+            team: club.team,
+            location: [club.latitude, club.longitude],
+            address: club.address,
+            icon: new Icon({ iconUrl: club.url_logo, iconSize: [46, 46] }),
+            url: "/clubpage/" + club.url,
+            areas: clubAreas,
+          };
+        });
         setMarkersData(transformedData);
       })
       .catch(err => {
@@ -64,6 +84,11 @@ const CustomMap = () => {
         <Popup>Your Localization</Popup>
       </Marker>
     );
+  };
+
+  const getClubAreas = (team) => {
+    const clubData = areasData.find(item => item.team.toLowerCase() === team.toLowerCase());
+    return clubData ? clubData.areas : [];
   };
 
   return (
@@ -109,7 +134,6 @@ const CustomMap = () => {
       </MapContainer>
     </div>
   );
-  
 };
 
 export default CustomMap;
