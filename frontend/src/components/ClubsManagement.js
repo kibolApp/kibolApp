0b9b24634d27405ec098c962d4a4c40a43b550b1
Tomasz Barczycki <115528520+TomaszBarczycki@users.noreply.github.com>
2024-01-09 18:29:12 +0100

    pagination-fixed

--- frontend/src/components/ClubsManagement.js
+++ frontend/src/components/ClubsManagement.js
@@ -261,12 +261,12 @@ const ClubManagement = () => {
         nextLabel={t('next')}
         pageCount={Math.ceil(clubs.length / clubsPerPage)}
         onPageChange={changePage}
+        activeClassName={"bg-custom-brown py-1 ml-[-1px] px-[-1px]"}
         containerClassName={"flex items-center justify-center mt-4"}
-        pageClassName={"px-3 py-1 mx-1 border rounded cursor-pointer transition duration-300 ease-in-out hover:bg-custom-olive hover:text-white"}
-        breakClassName="px-3 py-1 mx-1 border rounded cursor-pointer transition duration-300 ease-in-out hover:bg-custom-olive hover:text-white"
-        previousClassName={"px-3 py-1 mx-1 border rounded cursor-pointer transition duration-300 ease-in-out hover:bg-custom-olive hover:text-white"}
-        nextClassName={"px-3 py-1 mx-1 border rounded cursor-pointer transition duration-300 ease-in-out hover:bg-custom-olive hover:text-white"}
-        activeClassName={"bg-custom-olive text-white"}
+        breakClassName="px-3 py-2 mx-1 text-white border rounded cursor-pointer transition duration-300 ease-in-out hover:bg-custom-olive hover:text-white"
+        pageLinkClassName={"px-3 py-2 mx-1 text-white border rounded cursor-pointer transition duration-300 ease-in-out hover:bg-custom-olive hover:text-white"}
+        nextLinkClassName={"px-3 py-2 mx-1 text-white border rounded cursor-pointer transition duration-300 ease-in-out hover:bg-custom-olive hover:text-white"}
+        previousClassName={"px-3 py-1 mx-1 text-white border rounded cursor-pointer transition duration-300 ease-in-out hover:bg-custom-olive hover:text-white"}
         initialPage={selectedPage}
         />
 
