dc59e424ce3e16b6354d4b78067f95f59fa4d5ed
Tomasz Barczycki <115528520+TomaszBarczycki@users.noreply.github.com>
2024-01-12 15:33:57 +0100

    fixed api 2

--- frontend/src/axiosClientWeb.js
+++ frontend/src/axiosClientWeb.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosClientWeb=axios.create({
-    baseURL: 'http://127.0.0.1:8000'
+    baseURL: 'http://api.kibolapp.cloud'
 })
 
 axiosClientWeb.interceptors.request.use((config)=>{
