2a35cbbd8b2a9f652e0273214ab6200e8bc5c4da
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2024-01-12 23:34:51 +0100

    Tests...

--- kibolApp/tests/Feature/MiedzLegnicaSeederTest.php
+++ kibolApp/tests/Feature/MiedzLegnicaSeederTest.php
@@ -10,14 +10,13 @@ class MiedzLegnicaSeederTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testItSeedsMiedzLegnicaTableWithExpectedata()
+    public function testItSeedsMiedzLegnicaTableWithExpectedData()
     {
         $this->seed(Seeder::class);
 
         $this->assertDatabaseCount('miedzlegnica', count($this->getExpectedData()));
         $this->assertDatabaseHas('miedzlegnica', $this->getExpectedData()[0]);
     }
-
 
     public function getExpectedData(): array
     {
@@ -27,6 +26,8 @@ class MiedzLegnicaSeederTest extends TestCase
                 'url_logo' => "https://i.imgur.com/AJ27Q2w.png",
                 'positive' => null,
                 'negative' => null,
+                'lat' => null,
+                'lng' => null,
             ],
         ];
 
@@ -45,6 +46,11 @@ class MiedzLegnicaSeederTest extends TestCase
             $additionalData[] = ['negative' => $club];
         }
 
+        foreach ($this->getAreaDatalat() as $club) {
+            $additionalData[] = ['lat' => $club];
+        }
+
+
         return $additionalData;
     }
 
@@ -62,5 +68,51 @@ class MiedzLegnicaSeederTest extends TestCase
             'Pogoń Szczecin', 'Radomiak Radom', 'Ruch Chorzów', 'Sandecja Nowy Sącz', 'Stal Rzeszów',
             'Stal Stalowa Wola', 'Widzew Łódź', 'Wisła Kraków', 'Zawisza Bydgoszcz', 'Zagłębie Lubin'
         ];
+    }
+
+    public function getAreaDatalat(): array
+    {
+        $coordinates = [
+            ['lat' => 51.43058279985263, 'lng' => 16.764053638002707],
+            ['lat' => 51.316852677165, 'lng' => 16.062588607537464],
+            ['lat' => 51.29251902511578, 'lng' => 15.622685789194179],
+            ['lat' => 51.168295391201866, 'lng' => 15.00744328221424],
+            ['lat' => 51.16327301965441, 'lng' => 14.993378563123457],
+            ['lat' => 51.15067557973444, 'lng' => 15.001136147409312],
+            ['lat' => 50.92481562880877, 'lng' => 14.866300863938164],
+            ['lat' => 50.87579712448823, 'lng' => 14.825085077865339],
+            ['lat' => 50.872028359013484, 'lng' => 14.91496352937628],
+            ['lat' => 50.872028359013484, 'lng' => 14.99879661557469],
+            ['lat' => 50.979543909162004, 'lng' => 15.009540784337958],
+            ['lat' => 51.000317752067474, 'lng' => 14.973280593357913],
+            ['lat' => 51.021099756985336, 'lng' => 15.009063844801062],
+            ['lat' => 51.01543112800729, 'lng' => 15.093205547516192],
+            ['lat' => 50.99087408438126, 'lng' => 15.135463081242506],
+            ['lat' => 51.02487918024369, 'lng' => 15.174202516000463],
+            ['lat' => 50.98332036441661, 'lng' => 15.183543074715828],
+            ['lat' => 51.000317752067474, 'lng' => 15.23443879884502],
+            ['lat' => 50.974844094639025, 'lng' => 15.278926047527307],
+            ['lat' => 50.92759505399502, 'lng' => 15.2645240092682],
+            ['lat' => 50.85019801978618, 'lng' => 15.35836621388998],
+            ['lat' => 50.77856557126782, 'lng' => 15.380089075447984],
+            ['lat' => 50.80682988609283, 'lng' => 15.436644283228162],
+            ['lat' => 50.73525647227311, 'lng' => 15.6942872496289],
+            ['lat' => 50.75031645406236, 'lng' => 15.813780090759224],
+            ['lat' => 50.66754004778622, 'lng' => 15.864656544386577],
+            ['lat' => 50.69574458687245, 'lng' => 15.97808792893241],
+            ['lat' => 50.64122952187694, 'lng' => 16.007829068160277],
+            ['lat' => 50.59803382376373, 'lng' => 16.019656924324778],
+            ['lat' => 50.66002140388838, 'lng' => 16.106323022700423],
+            ['lat' => 50.62995763450729, 'lng' => 16.210557649060405],
+            ['lat' => 50.66754004778622, 'lng' => 16.22272662161629],
+            ['lat' => 50.66754004778622, 'lng' => 16.330147644785058],
+            ['lat' => 50.58301760756069, 'lng' => 16.448572574328324],
+            ['lat' => 50.526745290611814, 'lng' => 16.39737066746028],
+            ['lat' => 50.421866239721595, 'lng' => 16.194423510298833],
+            ['lat' => 50.40316012013895, 'lng' => 16.654356907197666],
+            ['lat' => 51.43058279985263, 'lng' => 16.764053638002707]
+        ];
+
+        return array_column($coordinates, 'lat');
     }
 }
---
+++ kibolApp/tests/Feature/PoloniaWarszawaSeederTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Tests\Feature\Seeders;
+
+use Tests\TestCase;
+use Database\Seeders\PoloniaWarszawaSeeder as Seeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PoloniaWarszawaSeederTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testItSeedsPoloniaWarszawaTableWithExpectedData()
+    {
+        $this->seed(Seeder::class);
+
+        $this->assertDatabaseCount('PoloniaWarszawa', count($this->getExpectedData()));
+        $this->assertDatabaseHas('PoloniaWarszawa', $this->getExpectedData()[0]);
+    }
+
+    public function getExpectedData(): array
+    {
+        $expectedData = [
+            [
+                'name' => "Polonia Warszawa",
+                'url_logo' => "https://i.imgur.com/L9znd4v.png",
+                'positive' => null,
+                'negative' => null,
+                'lat' => null,
+                'lng' => null,
+            ],
+        ];
+
+        return array_merge($expectedData, $this->getAdditionalData());
+    }
+
+    public function getAdditionalData(): array
+    {
+        $additionalData = [];
+
+        foreach ($this->getPositiveClubs() as $club) {
+            $additionalData[] = ['positive' => $club];
+        }
+
+        foreach ($this->getNegativeClubs() as $club) {
+            $additionalData[] = ['negative' => $club];
+        }
+
+        foreach ($this->getAreaDatalat() as $club) {
+            $additionalData[] = ['lat' => $club];
+        }
+
+
+        return $additionalData;
+    }
+
+    public function getPositiveClubs(): array
+    {
+        return [];
+    }
+
+    public function getNegativeClubs(): array
+    {
+        return [
+            "Legia Warszawa", "Zagłębie Sosnowiec", "Olimpia Elbląg", "Radomiak Radom", "Hutnik Warszawa",
+            "Olimpia Warszawa", "Ursus Warszawa", "Dolcan Ząbki", "Znicz Pruszków", "Świt Nowy Dwór Mazowiecki",
+            "Legionovia Legionowo", "Pogoń Siedlce", "Wisła Płock", "Legia Chełmża", "Pogoń Szczecin",
+            "Bałtyk Gdynia", "Jagiellonia Białystok", "Motor Lublin", "Hetman Zamość", "Wisła Puławy",
+            "KSZO Ostrowiec Św.", "Unia Tarnów", "Ruch Chorzów", "GKS Katowice", "BKS Stal Bielsko-Biała",
+            "Raków Częstochowa", "Gwardia Koszalin"
+        ];
+    }
+
+    public function getAreaDatalat(): array
+    {
+        $coordinates = [
+            ["lat" => 52.147285464629306, "lng" => 20.63063092773467],
+            ["lat" => 52.20043099413979, "lng" => 20.831509719447297],
+            ["lat" => 52.232854268718, "lng" => 20.95895204731832],
+            ["lat" => 52.26412186260367, "lng" => 20.96369949243885],
+            ["lat" => 52.2308816688101, "lng" => 21.024284392492547],
+            ["lat" => 52.2276330237157, "lng" => 21.04486187708227],
+            ["lat" => 52.304389456264715, "lng" => 21.15552563208493],
+            ["lat" => 52.459079070696845, "lng" => 21.547890950054892],
+            ["lat" => 52.65778034032817, "lng" => 22.448544740106456],
+            ["lat" => 52.80067874677539, "lng" => 22.421581638098218],
+            ["lat" => 52.89042550663413, "lng" => 22.273082449648314],
+            ["lat" => 53.06962901749628, "lng" => 21.895103681832182],
+            ["lat" => 53.12645627821391, "lng" => 21.732308282409974],
+            ["lat" => 53.02392303468994, "lng" => 21.044013751033674],
+            ["lat" => 52.805471776229695, "lng" => 20.855977063986415],
+            ["lat" => 52.497739068419776, "lng" => 20.319068695152424],
+            ["lat" => 52.147285464629306, "lng" => 20.63063092773467],
+        ];
+
+        return array_column($coordinates, 'lat');
+    }
+}
