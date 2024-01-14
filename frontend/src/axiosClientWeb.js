61de9d7812777cbe8e3d43afa91c4804e923e5a5
Tomasz Barczycki <115528520+TomaszBarczycki@users.noreply.github.com>
2024-01-14 20:20:13 +0100

    fix1

--- frontend/src/axiosClientWeb.js
+++ frontend/src/axiosClientWeb.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosClientWeb=axios.create({
-    baseURL: 'http://127.0.0.1:8000'
+    baseURL: 'http://localhost:8000'
 })
 
 axiosClientWeb.interceptors.request.use((config)=>{
