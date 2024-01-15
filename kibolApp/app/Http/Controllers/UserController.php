bb29ced4cc8239ceb4317478f0495e22de3c1c91
Tomasz Barczycki <115528520+TomaszBarczycki@users.noreply.github.com>
2024-01-15 11:39:41 +0100

    usercontrollerfix

--- kibolApp/app/Http/Controllers/UserController.php
+++ kibolApp/app/Http/Controllers/UserController.php
@@ -37,6 +37,8 @@ class UserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'role' => $request->role,
+
         ]);
 
         return response()->json($user, 201);
